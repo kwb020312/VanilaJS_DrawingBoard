da70e13a2868ef3dcc2e2ba7ad91ff68ad6e7754
kwb020312 <kwb020312@naver.com>
2022-03-28 22:44:27 +0900

    #2.3 Changing Color

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
 
-ctx.fillStyle = '#2c2c2c';
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -19,7 +23,7 @@ function onMouseMove(event) {
 	const y = event.offsetY;
 	if (painting) {
 		ctx.lineTo(x, y);
-		// ctx.strokeStyle();
+		ctx.stroke();
 	} else {
 		ctx.beginPath();
 		ctx.moveTo(x, y);
@@ -29,6 +33,13 @@ function onMouseMove(event) {
 function onMouseDown(event) {
 	painting = true;
 }
+
+function handleColorClick(e) {
+	const color = e.target.style.backgroundColor;
+	ctx.strokeStyle = color;
+}
+
+Array.from(colors).forEach((color) => color.addEventListener('click', handleColorClick));
 
 if (canvas) {
 	canvas.addEventListener('mousemove', onMouseMove);
